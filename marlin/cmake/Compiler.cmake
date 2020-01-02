6255dc95c380f3143913da39ca656755d58dfb9c
mcpca <mca@netc.eu>
2020-01-02 15:10:59 +0000

    cmake: Add icc-specific optimization options
    Added optimization options for Skylake and Knights Landing targets built
    on icc, as well as an option for compiling with Intel MKL support.

--- marlin/cmake/Compiler.cmake
+++ marlin/cmake/Compiler.cmake
@@ -2,3 +2,32 @@ include(CheckCXXCompilerFlag)
 
 check_cxx_compiler_flag("-march=native" MARLIN_COMPILER_SUPPORTS_MARCH_NATIVE)
 check_cxx_compiler_flag("-mtune=native" MARLIN_COMPILER_SUPPORTS_MTUNE_NATIVE)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+  if(MARLIN_INTEL_TARGET STREQUAL "SKX")
+    check_cxx_compiler_flag("-xMIC-AVX512"
+                            MARLIN_COMPILER_SUPPORTS_INTEL_MIC_AVX512)
+
+    if(MARLIN_COMPILER_SUPPORTS_INTEL_MIC_AVX512)
+      set(MARLIN_USE_INTEL_MIC_AVX512 ON)
+    endif()
+
+  elseif(MARLIN_INTEL_TARGET STREQUAL "KNL")
+    check_cxx_compiler_flag("-xCORE-AVX512"
+                            MARLIN_COMPILER_SUPPORTS_INTEL_CORE_AVX512)
+
+    if(MARLIN_COMPILER_SUPPORTS_INTEL_CORE_AVX512)
+      set(MARLIN_USE_INTEL_CORE_AVX512 ON)
+    endif()
+  endif()
+
+  if(MARLIN_USE_INTEL_MKL)
+    check_cxx_compiler_flag("-mkl" MARLIN_COMPILER_SUPPORTS_INTEL_MKL)
+
+    if(NOT MARLIN_COMPILER_SUPPORTS_INTEL_MKL)
+      set(MARLIN_USE_INTEL_MKL OFF)
+    endif()
+  endif()
+else()
+  set(MARLIN_USE_INTEL_MKL OFF)
+endif()
