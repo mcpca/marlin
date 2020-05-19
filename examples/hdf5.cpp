e692387ad02edb372f5b1be4d59b3797dcd489a9
mcpca <mca@netc.eu>
2020-05-19 10:05:08 +0100

    examples: Use HDF5's C++ bindings directly.

--- examples/hdf5.cpp
+++ examples/hdf5.cpp
@@ -1,23 +1,50 @@
 #include <array>
+#include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-#include <h5cpp/hdf5.hpp>
+#include <H5Cpp.h>
 
 #include "hdf5.hpp"
 #include "marlin/defs.hpp"
 
 namespace h5io
 {
+    // Safe conversion from type to H5T.
+    template<typename T>
+    constexpr H5::PredType get_type();
+
+    template<>
+    H5::PredType get_type<float>()
+    {
+        return H5::PredType::NATIVE_FLOAT;
+    }
+
+    template<>
+    H5::PredType get_type<double>()
+    {
+        return H5::PredType::NATIVE_DOUBLE;
+    }
+
+    template<>
+    H5::PredType get_type<long double>()
+    {
+        return H5::PredType::NATIVE_LDOUBLE;
+    }
+
     dataset_t read(std::string const& filename, std::string const& dsetname)
     {
-        auto f = hdf5::file::open(filename, hdf5::file::AccessFlags::READONLY);
+        auto f = H5::H5File(filename, H5F_ACC_RDONLY);
 
-        auto dataset = f.root().get_dataset(dsetname);
-        auto dataspace = hdf5::dataspace::Simple(dataset.dataspace());
-        auto dimensions = dataspace.current_dimensions();
+        auto dataset = f.openDataSet(dsetname);
+        auto dataspace = dataset.getSpace();
 
-        if(dimensions.size() != marlin::dim)
+        auto const n_dims = dataspace.getSimpleExtentNdims();
+        std::vector<hsize_t> dimensions(n_dims);
+        dataspace.getSimpleExtentDims(dimensions.data());
+
+        if(n_dims != marlin::dim)
         {
             throw std::runtime_error(
                 "Dataset has an invalid number of dimensions.");
@@ -27,7 +54,7 @@ namespace h5io
 
         dataset_t rv;
 
-        for(auto i = 0ul; i < dimensions.size(); ++i)
+        for(auto i = 0ul; i < static_cast<unsigned long>(n_dims); ++i)
         {
             rv.size[i] = dimensions[i];
 
@@ -37,23 +64,32 @@ namespace h5io
             }
         }
 
+        auto const n_points = dataspace.getSimpleExtentNpoints();
+
         msg += std::to_string(dimensions.back()) + ") (total " +
-               std::to_string(dataspace.size()) + ")";
+               std::to_string(n_points) + ")";
 
-        std::cout << msg << '\n';
+        std::cerr << msg << '\n';
 
-        rv.data = std::vector<marlin::scalar_t>(dataspace.size());
-        dataset.read(rv.data);
+        rv.data = std::vector<marlin::scalar_t>(n_points);
+        dataset.read(rv.data.data(), get_type<marlin::scalar_t>());
 
-        std::cout << "Successfully read dataset \'" + dsetname + "\'." << '\n';
+        std::cerr << "Successfully read dataset \'" + dsetname + "\'." << '\n';
 
         return rv;
     }
 
-    static bool dset_exists(hdf5::file::File const& f,
+    static bool dset_exists(H5::H5File const& f,
                             std::string const& dsetname)
     {
-        return f.root().has_dataset(dsetname);
+        if (!f.nameExists(dsetname))
+            return false;
+        try {
+            f.openDataSet(dsetname);
+            return true;
+        } catch (...) {
+            return false;
+        }
     }
 
     void write(std::string const& filename,
@@ -61,7 +97,7 @@ namespace h5io
                std::vector<marlin::scalar_t> const& data,
                marlin::point_t const& size)
     {
-        auto f = hdf5::file::open(filename, hdf5::file::AccessFlags::READWRITE);
+        auto f = H5::H5File(filename, H5F_ACC_RDWR);
 
         if(dset_exists(f, dsetname))
         {
@@ -69,16 +105,12 @@ namespace h5io
                                      "\' already exists!");
         }
 
-        auto dims = hdf5::Dimensions(size.size());
+        std::vector<hsize_t> dims(size.size());
         std::copy(std::begin(size), std::end(size), std::begin(dims));
+        auto const space = H5::DataSpace(dims.size(), dims.data());
+        auto const type = get_type<marlin::scalar_t>();
 
-        auto dataset =
-            hdf5::node::Dataset(f.root(),
-                                dsetname,
-                                hdf5::datatype::create<marlin::scalar_t>(),
-                                hdf5::dataspace::Simple(dims));
-
-        dataset.write(data);
+        auto dataset = f.createDataSet(dsetname, type, space);
+        dataset.write(data.data(), type, space);
     }
 }    // namespace h5io
-
