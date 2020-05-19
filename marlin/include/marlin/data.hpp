92846cd37e6edc949174f4e540490000ec8c7ea9
mcpca <mca@netc.eu>
2020-05-19 15:26:52 +0100

    Fix documentation.

--- marlin/include/marlin/data.hpp
+++ marlin/include/marlin/data.hpp
@@ -45,7 +45,6 @@ namespace marlin
 
             //! @brief Construct from preexisting data.
             //
-            //! @param memsize number of datapoints.
             //! @param values data array.
             explicit data_t(std::vector<scalar_t>&& values) noexcept
                 : m_values(std::move(values)), m_memsize(m_values.size())
